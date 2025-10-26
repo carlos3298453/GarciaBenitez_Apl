9e2accc9cf6915a3c8fc1f154d64da143576adef
carlos3298453 <carlos00221990@gmail.com>
2025-10-25 22:31:27 -0600

    tarea-jueves-23

--- GarciaBenitez_20251016/app/Http/Controllers/ZonaController.php
+++ GarciaBenitez_20251016/app/Http/Controllers/ZonaController.php
@@ -54,8 +54,8 @@ class ZonaController extends Controller
         return response()->Json($respuesta, 200);
     }
 
-    public function obtenerZona($idzona){
-
+    public function obtenerZona($idzona): \Illuminate\Http\JsonResponse
+    {
         $satisfactorio = false;
         $estado = 0;
         $mensaje = "";
@@ -107,5 +107,116 @@ class ZonaController extends Controller
         ];
 
         return response()->Json($respuesta, 200);
+    }
+
+    public function obtenerZonaPais(int $idpais = 0): \Illuminate\Http\JsonResponse
+    {
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores = [];
+
+        if($idpais > 0) {
+            $Zona = new Zona();
+            $valores = $Zona->where('id_pais', $idpais)->get();
+
+            if(!empty($valores)) {
+                $satisfactorio = true;
+                $estado = 200;
+                $mensaje = "Valores encontrados";
+                $errores = [
+                    "code" => 200,
+                    "msg" => ""
+                ];
+            }
+            // No se encontraron datos
+            else {
+                $satisfactorio = false;
+                $estado = 404;
+                $mensaje = "No se han encontrado valores";
+                $errores = [
+                    "code" => 404,
+                    "msg" => "Datos no encontrados"
+                ];
+            }
+        }
+        else {
+            // No se ha enviado un valor para el parámetro $idpais
+            $satisfactorio = false;
+            $estado = 400;
+            $mensaje = "No se ha enviado el parámetro obligatorio";
+            $errores = [
+                "code" => 400,
+                "msg" => "El identificador del país está vacío"
+            ];
+        }
+
+        $respuesta = [
+            "success" => $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
+            "data" => $valores,
+            "errors" => $errores,
+            "total" => sizeof($valores)
+        ];
+
+        return response()->Json($respuesta, 200);
+    }
+
+    public function crearZona(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $satisfactorio = false;
+        $estado = 0;
+        $mensaje = "";
+        $errores = [];
+        $valores = [];
+
+        $validacion = $request->validate([
+            "idpais" => "required|integer|gt:0",
+            "nombrezona" => "required|max:50"
+        ]);
+
+        $Zona = new Zona();
+
+        $Zona->id_pais = $request->idpais;
+        //      ^(BD)              ^(Form)
+        $Zona->nombre_zona = $request->nombrezona;
+        //      ^(BD)                   ^(Form)
+
+        $insertado = $Zona->save();
+
+        if($insertado) {
+            $ultimoinsertado = $Zona->id;
+            $datosinsertados = $this->obtenerZona($ultimoinsertado);
+
+            $satisfactorio = true;
+            $estado = 200;
+            $mensaje = "Se guardaron los datos correctamente";
+            $errores = [
+                "code" => 200,
+                "msg" => ""
+            ];
+        }
+        else {
+            $satisfactorio = false;
+            $estado = 500;
+            $mensaje = "Hubo un problema al guardar los datos";
+            $errores = [
+                "code" => 500,
+                "msg" => "No se pudo hacer insert a la tabla Zona"
+            ];
+        }
+
+        $respuesta = [
+            "success" => $satisfactorio,
+            "status" => $estado,
+            "msg" => $mensaje,
+            "data" => $valores,
+            "errors" => $errores,
+            "total" => sizeof($valores)
+        ];
+
+        return response()->Json($respuesta, $estado);
     }
 }
