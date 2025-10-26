216f45991dd9d990c1d05456b55f81d93d7672a3
carlos3298453 <carlos00221990@gmail.com>
2025-10-25 21:57:08 -0600

    tarea-jueves-23

--- GarciaBenitez_20251016/routes/api.php
+++ GarciaBenitez_20251016/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZonaController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +12,11 @@ Route::get('/user', function (Request $request) {
 Route::get('/zonas', [zonaController::class, 'obtenerZonas']);
 
 Route::get('/zona/{idzona}', [zonaController::class, 'obtenerZona']);
+
+Route::get('/zonaspais/{idpais}', [zonaController::class, 'obtenerZonaPais']);
+
+Route::post('/nuevazona', [zonaController::class, 'crearZona']);
+
+Route::post('/nuevousuario', [AuthController::class, 'nuevoUsuario']);
+
+Route::post('/login', [AuthController::class, 'login']);
